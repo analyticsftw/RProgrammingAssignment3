f281ada31ea3d73b2c7d3bc727b53fb49135c7fd
Julien Coquet <julien.coquet@gmail.com>
2020-05-19 19:39:36 +0200

    updated sort

--- assignment.R
+++ assignment.R
@@ -1,1 +1,12 @@
 # Programming Assignment #3
+# Plot the 30-day mortality rate for heart attack
+
+# Load data - outcomes
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+# Get all figures for column #11
+outcome[, 11] <- as.numeric(outcome[, 11])
+
+# Plot histogram
+label <- "30-day death rates from heart attack"
+hist(outcome[, 11], main = label)
--- best.R
+++ best.R
@@ -3,47 +3,64 @@
 best <- function(state="AL", outcome="heart attack") {
   # Inputs default to "AL" for state and "heart attack" for outcome
   
+  # If state.abb is available, check for state validity
+  if (!is.null(state.abb)){
+    # we have state.abb support
+    if(!state %in% state.abb == TRUE)
+      stop("Invalid state.")
+  }
+  
   # Vector mapping
   allowedOutcomes <- c('heart attack'=1,'heart failure'=1,'pneumonia'=1)
-  outcomeColumn=c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
+  #outcomeColumn=c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
+  
+  outcomeColumn=c(
+    "heart attack"="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
+    "heart failure"="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
+    "pneumonia"="Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+  )
   
   # Exit program if outcome not allowed
   if (is.na(allowedOutcomes[outcome])){
-      errorMessage = paste("Outcome",outcome, "not allowed; use one of the following: 'heart attack','heart failure','pneumonia'")
-      stop(errorMessage)
+      #errorMessage = paste("Outcome",outcome, "not allowed; use one of the following: 'heart attack','heart failure','pneumonia'")
+      stop("Invalid outcome.")
   }
 
-  # Pre-run details
-  
   ## Read outcome data
   hospitals <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
   ## Check that state and outcome are valid
   statesMatch <- hospitals$State==state
-  dataByState <- hospitals[statesMatch,]
+  stateData <- hospitals[statesMatch,]
   
-  nres = nrow(dataByState)
+  nres = nrow(stateData)
   if (nres>0){
     # We have records!
   } else {
     # We don't have records. Output error message and quit
-    stop(paste("No data for state ",state))
+    stop(paste("No data for",state))
   }
-  print(paste("Found",nres,"records for",state,"state"))
   
   ## Return hospital name in that state with lowest 30-day death
   
   # Select outcome column
   selectedOutcome <- outcomeColumn[outcome]
   
-  ## rate
-  sortedHospitals <- dataByState[order(dataByState[,outcomeColumn[outcome]]),]
-  sortedHospitals <- data.frame(sortedHospitals$Hospital.Name,sortedHospitals[selectedOutcome])
+  ## Clean & sort
+  # Remove NAs
+  #stateOutcomeData <- stateData[]
+  stateData <- stateData[selectedOutcome]!='Not Available'
+
   
-  # Rename columns
-  names(sortedHospitals) <- c('Hospital name', "Deaths")
+  # Sort
+  sortedHospitals <- stateData[order(round(as.numeric(stateData[selectedOutcome])))]
+  #sortedHospitals <- data.frame(sortedHospitals$Hospital.Name,sortedHospitals[selectedOutcome])
   
-  # Print top result
-  print (paste("Hospital(s) in",state,"with lowest 30-day deaths for",outcome))
+  # Rename columns (optional)
+  #names(sortedHospitals) <- c('Hospital name', "Deaths")
+  
+  ## Print top result 
+  #print (paste("Hospital(s) in",state,"with lowest 30-day deaths for",outcome))
+  #head(sortedHospitals,1)
   head(sortedHospitals,1)
 }
